cf39ccf3514bdacee84514d15a46409660877b29
Your Name <you@example.com>
2020-04-12 20:19:50 +0000

    Fixed

--- scripts/assign-vip.sh
+++ scripts/assign-vip.sh
@@ -1,14 +1,16 @@
+#!/bin/bash
 VIP=$1
 mkdir -p /etc/assign-vip
 #Check if the VIP is being used
 IP=`gcloud compute addresses list| grep $VIP | awk '{ print $2 }'`
 while true; do
-  while [ $(gcloud compute addresses list| grep $VIP | awk '{ print $8 }') == 'IN_USE'; do
+  while [[ $(gcloud compute addresses list --filter="name=$VIP"| grep $VIP | awk '{ print $NF }') == "IN_USE" ]];
+  do
     echo "IP address in use at $(date)" >> /etc/assign-vip/poll.log
     sleep 30
   done
 
-  ZONE=`gcloud compute instances list | grep $(hostname) | awk '{ print $2 }'`
+  ZONE=`gcloud compute instances list --filter="name=$(hostname)"| grep $(hostname) | awk '{ print $2 }'`
   # Assign IP aliases to me because now I am the MASTER!
   gcloud compute instances network-interfaces update $(hostname) \
     --zone $ZONE \
