5d4e0f901d5cc233b0e6eeb3d6a88928fe7fc75f
Agarwal <gaurav.a.agarwal@capgemini.com>
2020-04-12 20:38:20 +0100

    Updated VIP

--- scripts/configure-assign-vip.sh
+++ scripts/configure-assign-vip.sh
@@ -1,6 +1,8 @@
+VIP=$1
 sudo rpm -ivh http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm
 sudo yum install -y nginx telnet
 sudo systemctl restart nginx
+sed -i "s/#VIP/${VIP}/g" assign-vip.service
 sudo cp -a assign-vip.service /etc/systemd/system/assign-vip.service
 sudo chmod +x assign-vip.sh
 sudo mv assign-vip.sh /usr/bin/assign-vip.sh
